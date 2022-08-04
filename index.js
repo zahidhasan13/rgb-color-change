575d412b4c0e0de2c033f6dd254eef244aa90f4b
zahidhasan13 <zahidhasanofficial13@gmail.com>
2022-08-04 18:21:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    main();
+}
+
+function main() {
+  const bodyColorChange = document.querySelector("body");
+  const colorChangeBtn = document.querySelector("#btn");
+
+  colorChangeBtn.addEventListener("click", function () {
+    const bgColor = rgbColor();
+    bodyColorChange.style.backgroundColor = bgColor;
+  });
+}
+
+const rgbColor = () => {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `rgb(${red},${green},${blue})`;
+};
